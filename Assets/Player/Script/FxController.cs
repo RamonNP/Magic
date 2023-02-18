a252144374c84567fa295880ace122b9345a8880
Ramon <ramon.pereira3156@gmail.com>
2023-02-18 17:03:14 -0300

    adicionado Shaders
    adicionado Shaders

--- Assets/Player/Script/FxController.cs
+++ Assets/Player/Script/FxController.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class FxController : MonoBehaviour
 {
-    [SerializeField] private GameObject _chargingEffects;
     [SerializeField] private MagicCastController _magicController;
+    [SerializeField] private Material _material;
+    Coroutine _coroutineChangeColorCharging;
 
+
+    private void Awake()
+    {
+        //StartCoroutine(ChangeColorCharging());
+    }
 
     private void Update()
     {
-        if (_magicController.Charging > 1.5f)
-        {
-            _chargingEffects.SetActive(true);
-        } else
-        {
-            _chargingEffects.SetActive(false);
-        }
+
     }
 
     public void PlayCharging()
     {
         
+    }
+
+    IEnumerator ChangeColorCharging()
+    {
+
+        Color c = _material.color;
+        while (true)
+        {
+            if (_magicController.Charging > 1.5f)
+            {
+                Debug.Log(_magicController.Charging);
+                c.b = 255;
+                c.g = 0;
+                c.r = 0;
+                _material.SetColor("_Tint",c);
+                yield return new WaitForSeconds(0.2f);
+                c.b = 0;
+                c.g = 0;
+                c.r = 255;
+                _material.SetColor("_Tint", c);
+                yield return new WaitForSeconds(0.2f);
+                c.b = 0;
+                c.g = 255;
+                c.r = 255;
+                _material.SetColor("_Tint", c);
+                yield return new WaitForSeconds(0.2f);
+                c.b = 255;
+                c.g = 0;
+                c.r = 255;
+                _material.SetColor("_Tint", c);
+                yield return new WaitForSeconds(0.2f);
+            } else
+            {
+                Debug.Log(_magicController.Charging);
+                c.b = 0;
+                c.g = 0;
+                c.r = 0;
+                _material.SetColor("_Tint", c);
+                yield return new WaitForSeconds(0.2f);
+            }
+            yield return ((_magicController.Charging > 1.5f));
+
+        }
+
     }
 }
