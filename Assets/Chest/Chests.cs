e25ed5857ffacdc312fdcc618a8ee6ca71ec786c
ArthurSPereira3156 <53497812+ArthurSPereira3156@users.noreply.github.com>
2023-03-03 17:40:04 -0300

    Chest
    Add Chests

--- Assets/Chest/Chests.cs
+++ Assets/Chest/Chests.cs
@@ -24,7 +24,7 @@ public class Chests : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             colPlayer = true;
         }
